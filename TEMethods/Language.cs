58b2727cee3015d7f1fc7b7bd623cdbb60fca28e
iceblade112 <iceblade112@hotmail.com>
2016-03-22 21:59:26 -0400

    Part of TEMethods. Encryption and buffer soon!

---
+++ TEMethods/Language.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickEmu
+{
+    public class Language
+    {
+        // English is default
+        public static string lang = "en";
+
+        // Defaults
+        public static Dictionary<string, string> strings = new Dictionary<string, string>();
+
+        public Language()
+        {
+            strings["PacketHandler"] = "Packet Handler";
+            strings["SocketSys"] = "Socket System";
+            strings["General"] = "General";
+            strings["ConfigNotExist"] = "Configuration file does not exist. Creating.";
+            strings["ConfigErrorUseDefault"] = "An error occurred while creating the default configuration file. Defaults will be used.";
+            strings["MySQLConnectError"] = "An error occurred while connecting to MySQL. Press any key to exit.";
+            strings["StartingServer"] = "Starting server...";
+            strings["StartedServer"] = "Server has been started on port {0}.";
+            strings["ErrorStartServer"] = "An error occurred while starting the server: ";
+            strings["UnhandledPacket"] = "Unhandled packet received";
+            strings["ForcefulDisconnect"] = "Client {0} disconnected forcefully.";
+            strings["ClientAccepted"] = "Client accepted from {0}.";
+        }
+
+        public static void loadFromFile(string lang)
+        {
+            if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\languages"))
+            {
+                if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\languages\\" + lang + ".ini"))
+                {
+                    var inifile = new ConfigReader(AppDomain.CurrentDomain.BaseDirectory + "\\languages\\" + lang + ".ini");
+
+                    foreach (string key in strings.Keys.ToArray())
+                    {
+                        if (inifile.KeyExists(key, "Strings"))
+                        {
+                            strings[key] = inifile.Read(key, "Strings");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TEMethods/Methods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickEmu
+{
+    public class Methods
+    {
+        public static string sep(string str, string delim)
+        {
+            int len = str.IndexOf(delim);
+            if (len > 0)
+            {
+                return str.Substring(0, len);
+            }
+            return "";
+        }
+
+        public static string getString(byte[] bytes, int i)
+        {
+            return Config.encoding.GetString(bytes);
+        }
+
+        public static void echoColor(string from, ConsoleColor color, string write)
+        {
+            echoColor(from, color, write, new string[] { });
+        }
+
+        public static void echoColor(string from, ConsoleColor color, string write, string[] args)
+        {
+            Console.ForegroundColor = color;
+            Console.Write("[" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + " | " + from + "] ");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i != args.Length; i++)
+            {
+                write = write.Replace("{" + i + "}", args[i]);
+            }
+            Console.WriteLine(write);
+        }
+
+        public static ushort ReadUshort(byte[] bytes, int pos)
+        {
+            byte[] ba = new byte[2] { bytes[pos], bytes[pos + 1] };
+            return BitConverter.ToUInt16(ba.Reverse().ToArray(), 0);
+        }
+
+        public static string cleanString(string str)
+        {
+            return str.TrimEnd(new char[] { (char)0 }).Replace("\x00", "").Replace("\u0000", "");
+        }
+
+        // From http://stackoverflow.com/questions/8041343/how-to-split-a-byte
+        // Author: driis http://stackoverflow.com/users/13627/driis
+        public static IEnumerable<byte[]> Split(byte splitByte, byte[] buffer)
+        {
+            List<byte> bytes = new List<byte>();
+            foreach (byte b in buffer)
+            {
+                if (b != splitByte)
+                    bytes.Add(b);
+                else
+                {
+                    yield return bytes.ToArray();
+                    bytes.Clear();
+                }
+            }
+            yield return bytes.ToArray();
+        }
+    }
+}
