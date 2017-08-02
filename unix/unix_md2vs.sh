46cb38e988715612713d8166391e696395cb6c95
PyroSamurai <pyrosamurai@member.fsf.org>
2017-08-01 22:48:29 -0400

    Add bash scripts for automated .csproj conversion for unix-like
    Automated scripts to convert .csproj files between VisualStudio and
    MonoDevelop format. Useful for those working on a Unix-like OS.

---
+++ unix/unix_md2vs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# MonoDevelop to Visual Studio .csproj converter
+# Usage: Run from TrickEmu directory with "sh unix/unix_md2vs.sh"
+
+## GNU sed (default for XGL (X11+GNU+Linux))
+find . -name '*.csproj' -exec sed -i 's/>v4.5</>v4.5.2</g' "{}" \;
+
+## Non-GNU sed (used on macOS. Its official name, as of 2016, I checked.)
+#find . -name '*.csproj' -exec sed -i '' 's/>v4.5</>v4.5.2</g' "{}" \;
